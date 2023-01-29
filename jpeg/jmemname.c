4041c0fbdb6388b3a3a348ef4472631aa746543f
Ger Hobbelt <ger@hobbelt.com>
2023-01-29 10:23:05 +0100

    For now: reverting the last patch as we move forward with a new jpeg API in our monolithic dev env.
    TODO: remove the jpeg code copy entirely, reference external repo for that.
    
    ----
    
    Commit message in the other libs that triggered this:
    
    redid the libjpeg/libjpeg-turbo API change to fix what was initially addressed in mupdf commit SHA-1: 4a694c3e9c31dc6f9dcc975b894bff469c30a5a3: application using multiple jpeg_mem_* implementations, while the jpeg/jpeg-turbo design only facilitates having one (globally known and statically linked functions jpeg_mem_*); now that has been changed by adding a CALLBACK function reference to the cinfo setup struct: when NULL (i.e. not specified), a default malloc+free+no_backstore implementation is used, which suits most users (= other libraries), while anyone with specific needs can thus provide a callback function which can then initialize the system interface for jpeg/jpeg-turbo.
    
    Compiles and tests okay in monolithic build mode for mutool & tesseract tools (where tesseract tool would previously crash).
    
    TODO:
    
    - adapt jpeg/jpeg-turbo documentation
    - implement in libjpeg: we were already mostly moved away from that one already, but now we introduced the 'nilled' branch for libjpeg where the library is essentially deleted in order to catch lingering search paths and other incorrect references or loads of that source: several of these mistakes have been fixed, but libjpeg SHOULD receive the same I/F API patch as we did for jpeg-turbo.
    - add an API to specify a 'global default' for the jpeg mem sys I/F, which would enable us to replace the default implementation (jpeg_nobs_mem_*) currently used by jpeg/jpeg-turbo when no custom override a.k.a. cinfo.client_callback setup function has been provided.

--- jpeg/jmemname.c
+++ jpeg/jmemname.c
@@ -12,8 +12,6 @@
  * is shoved onto the user.
  */
 
-#if !defined(BUILD_MONOLITHIC)
-
 #define JPEG_INTERNALS
 #include "jinclude.h"
 #include "jpeglib.h"
@@ -276,5 +274,3 @@ jpeg_mem_term (j_common_ptr cinfo)
 {
   /* no work */
 }
-
-#endif // !defined(BUILD_MONOLITHIC)
